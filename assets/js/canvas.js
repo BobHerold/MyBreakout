0121375a47651d857e57bc53ddf6d3e907b874de
Bob Herold <54417085+BobHerold@users.noreply.github.com>
2020-07-06 14:26:28 -0400

    experimented with styling the canvas element from style.css, etc.  It can't be done in that file, as canvas is an HTML5 element and must be set in index.html.  Also experimented with ball changes in canvas.js file....etc.    All changes related to each other..

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,8 +1,20 @@
 var canvas = document.getElementById("theCanvas");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");  // this variable is the actual tool that allows us to paint on the canvas//
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(canvas.width/2, canvas.height/2, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
 ctx.closePath();
