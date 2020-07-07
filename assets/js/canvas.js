669bf85dc6f222ef06db1a3e973e29e6a8a1ba33
Bob Herold <54417085+BobHerold@users.noreply.github.com>
2020-07-07 16:53:31 -0400

    "eliminated the theCanvas ID from style.css as it would not work there. Added the functions draw, and drawBall w/parameters to canvas.js file"

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,20 +1,25 @@
 var canvas = document.getElementById("theCanvas");
 var ctx = canvas.getContext("2d");  // this variable is the actual tool that allows us to paint on the canvas//
+var x = canvas.width/2;
+var y = canvas.height -30;  // the x and y variables define a starting point for the ball at bottom center of the canvas//
+var dx = 2;
+var dy = -2;  //these variables give a small value to the x & y variables after every frame has been drawn on canvas, to make the ball move//
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+//below is the function that will draw the ball on our canvas//
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2); //this sets the x and y axis of ball, its radius and circumference, and starting angle//
+    ctx.fillStyle = "#f56038" //color of the ball//
+    ctx.fill();
+    ctx.closePath();
+    
+}
+// the below function clears the canvas, and calls for it to redraw the ball in a new position every 10 miliseconds//
+function draw(){
+    ctx.clearRect(0,0, canvas.width, canvas.height); //this clears the canvas content after each frame is run so that you don't see the previous position of the ball//
+    drawBall();
+    x += dx;
+    y += dy;
 
-ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/2, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+}
+setInterval (draw, 10);
