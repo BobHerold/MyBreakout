19ef23b6d4f4a6de4344228fd9686a50c96f7753
Bob Herold <54417085+BobHerold@users.noreply.github.com>
2020-07-08 15:58:36 -0400

    Added collision detection "if" statements for the ball to bounce off of x and y canvas parameters and reverse direction if collision detected to function "drawBall"..

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -4,11 +4,12 @@ var x = canvas.width/2;
 var y = canvas.height -30;  // the x and y variables define a starting point for the ball at bottom center of the canvas//
 var dx = 2;
 var dy = -2;  //these variables give a small value to the x & y variables after every frame has been drawn on canvas, to make the ball move//
+var ballRadius = 10; //holds the radius of the drawn ball and is used for calculations//
 
 //below is the function that will draw the ball on our canvas//
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2); //this sets the x and y axis of ball, its radius and circumference, and starting angle//
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2); //this sets the x and y axis of ball, its radius and circumference, and starting angle, actually draws the circle of the ball//
     ctx.fillStyle = "#f56038" //color of the ball//
     ctx.fill();
     ctx.closePath();
@@ -18,6 +19,13 @@ function drawBall() {
 function draw(){
     ctx.clearRect(0,0, canvas.width, canvas.height); //this clears the canvas content after each frame is run so that you don't see the previous position of the ball//
     drawBall();
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            dx = -dx;    // this formulates whether the ball has hit the left or right walls of the canvas, if so, it reverses the ball's direction//
+        }
+        if(y + dy > canvas.height-ballRadius  || y + dy < ballRadius) {
+            dy = -dy;  //this formulates whether the ball has collided with the top or bottom of the canvas, and then reverses the ball's direction//
+        }  //Both of the above if statements are our "collision detection"//
+    
     x += dx;
     y += dy;
 
